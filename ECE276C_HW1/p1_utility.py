finish jacobian and test, pep8 fix

ECE276C_HW1/p1_utility.py
@@ -6,7 +6,6 @@ Utility functions for p1
 
 import numpy as np
 
-
 # define links
 L0 = 0.1
 L1 = 0.11
@@ -15,7 +14,7 @@ L1 = 0.11
 def getDhMatrix(alpha, a, theta, d):
     """
     Calculate D-H matrix using D-hH parameters
-    
+
     :param alpha: float, alpha(i-1) in D-H parameter (rad)
     :param a: float, a(i-1) in D-H parameter (m)
     :param theta: float, theta in D-H parameter (rad)
@@ -23,44 +22,50 @@ def getDhMatrix(alpha, a, theta, d):
     :return: 4 x 4 np array, D-H matrix
     """
     return np.array([[np.cos(theta), -np.sin(theta), 0, a],
-                     [np.sin(theta)*np.cos(alpha), np.cos(theta)*np.cos(alpha), -np.sin(alpha), -np.sin(alpha)*d],
-                     [np.sin(theta)*np.sin(alpha), np.cos(theta)*np.sin(alpha), np.cos(alpha), np.cos(alpha)*d],
+                     [np.sin(theta)*np.cos(alpha), np.cos(theta) *
+                      np.cos(alpha), -np.sin(alpha), -np.sin(alpha)*d],
+                     [np.sin(theta)*np.sin(alpha), np.cos(theta) *
+                      np.sin(alpha), np.cos(alpha), np.cos(alpha)*d],
                      [0, 0, 0, 1]])
 
 
 def getForwardModel(q0, q1):
     """
     Calculate forward kinematics using D-H parameters,
-    return end effector position
-    
+    return end effector position and orientation
+
     :param q0: float, central joint angle (rad)
     :param q1: float, elbow joint angle (rad)
-    :return: np array, [end effector x position, end effector y position] (m)
+    :return: np array, [end effector x position (m), end effector y position (m), end effector orientation (theta)] (m)
     """
     # define D-H parameters
     # columns: alpha, a, theta, d
     dh_parameters = np.array([[0, 0, q0, 0],
                               [0, L0, q1, 0],
                               [0, L1, 0, 0]])
-    
+
     # calculate D-H matrices
     dh_matrix_list = []
     for i in range(dh_parameters.shape[0]):
         dh_matrix_list.append(getDhMatrix(dh_parameters[i, 0],
-                                                dh_parameters[i, 1],
-                                                dh_parameters[i, 2],
-                                                dh_parameters[i, 3]))
+                                          dh_parameters[i, 1],
+                                          dh_parameters[i, 2],
+                                          dh_parameters[i, 3]))
 
     # calculate transformation matrix
     tf_matrix = dh_matrix_list[0]@dh_matrix_list[1]@dh_matrix_list[2]
-    
-    return np.array([tf_matrix[0, 3], tf_matrix[1, 3]])
-    
+
+    return np.array([tf_matrix[0, 3], tf_matrix[1, 3], np.arccos(tf_matrix[0, 0])])
+
 
 def getJacobian(q0, q1):
     """
+    Jacobian matrix is obtained by matlab helper script
     """
-    pass
+    return np.array([[- np.sin(q0)/10 - (11*np.cos(q0)*np.sin(q1))/100 - (11*np.cos(q1)*np.sin(q0))/100, - (11*np.cos(q0)*np.sin(q1))/100 - (11*np.cos(q1)*np.sin(q0))/100],
+                     [np.cos(q0)/10 + (11*np.cos(q0)*np.cos(q1))/100 - (11*np.sin(q0)*np.sin(q1)) /
+                      100, (11*np.cos(q0)*np.cos(q1))/100 - (11*np.sin(q0)*np.sin(q1))/100],
+                     [1, 1]])
 
 
 def getIK(x, y):

ECE276C_HW1/tests/test_p1.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Unit tests for p1
+Please use pytest to run this script automatically
 """
+
 
 import sys
 import logging
 import numpy as np
-
 import gym
 try:
     import pybulletgym.envs
@@ -15,29 +16,54 @@ except Exception as err:
     logging.warning('Should not reload pybulletgym: {}.'.format(err))
 
 sys.path.append('../')
+from p1_utility import getForwardModel, getJacobian
 from p1_env import ReacherEnv
-from p1_utility import getForwardModel
 
 
 def testGetForwardModel():
-    print('Testing getForwardModel()')
+    print('\n=====Testing getForwardModel()=====\n')
     env = gym.make("ReacherPyBulletEnv-v0")
     env.reset()
     robot = ReacherEnv(env)
-    
+
     for _ in range(5):
         joint_angle = np.random.rand(2)
-    
-        # verfy using environment observation
-        robot.setJointPosition(joint_angle[0], joint_angle[1])
-        ee_position_calculated = getForwardModel(joint_angle[0], joint_angle[1])[:2]
-        ee_position_env = env.unwrapped.robot.fingertip.get_position()[:2]
-        
-        print('Joint angle: {}\nCalculated end effector position: {}\nEnv end effector position: {}\n'\
-              .format(joint_angle, ee_position_calculated, ee_position_env))
-        assert (ee_position_calculated.round(2) == ee_position_env.round(2)).all()
 
-    
+        # verify using environment observation
+        robot.setJointPosition(joint_angle[0], joint_angle[1])
+        # don't know how to test theta, env return nonsense value
+        ee_position_calculated = getForwardModel(
+            joint_angle[0], joint_angle[1])[:2]
+        ee_position_env = env.unwrapped.robot.fingertip.get_position()[:2]
+        # ee_orientation_env = env.unwrapped.robot.fingertip.get_orientation()
+
+        print('Joint angle: {}\nCalculated end effector position: {}\nEnv end effector position: {}'
+              .format(joint_angle, ee_position_calculated, ee_position_env))
+        assert (ee_position_calculated.round(3)
+                == ee_position_env.round(3)).all()
+
+
+def testGetJacobian():
+    print('\n=====Testing getJacobian()=====\n')
+    env = gym.make("ReacherPyBulletEnv-v0")
+    env.reset()
+    robot = ReacherEnv(env)
+
+    # Because for some reason, gym always returns 0 tip velocity even we input joint velocity,
+    # here we define a simple test case to test Jacobian
+    joint_angle = np.array([0, 0])
+    jacobian_matrix = getJacobian(joint_angle[0], joint_angle[1])
+    joint_velocity = np.array([0.5, 0])
+
+    x_dot_calculated = jacobian_matrix@joint_velocity
+    print('Joint angle: {}\nJoint velocity: {}\n'
+          'Calculated end effector velocity: {}'
+          .format(joint_angle, joint_velocity, x_dot_calculated))
+    assert (x_dot_calculated.round(3) == np.array(
+        [0, 0.21*0.5, 0.5]).round(3)).all()
+
+
 if __name__ == '__main__':
     testGetForwardModel()
-    logging.error('Please use pytest to run test script')
+    testGetJacobian()
+    print('All test passed')