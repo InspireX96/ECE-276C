f9a5d5a7310a9ca3e15a3f80fafbf1602a08ee11
Mingwei Xu <xumw1996@Mingweis-MacBook-Pro.local>
2019-10-10 22:37:15 -0700

    finish joint PD control

--- ECE276C_HW1/p1_controller.py
+++ ECE276C_HW1/p1_controller.py
@@ -3,11 +3,8 @@
 """
 PD controllers for p1
 """
-import logging
 import numpy as np
-from matplotlib import pyplot as plt
 
-from p1_env import ReacherEnv
 from p1_utility import getForwardModel, getJacobian, getIK
 
 
@@ -25,12 +22,13 @@ class ReacherEnvController():
         self.Kd = Kd
         # define register for D control diff
         self._state_error_memory = np.zeros(2)
+        self._q_error_memory = np.zeros(2)
 
     def pdControlEndEffector(self, state_err, q0, q1, dt):
         """
-        PD controller using teh error in the end-effector
+        PD controller using error in the end-effector
 
-        :param state_err: np array (len=2), state error
+        :param state_err: np array (len=2), end effector error
         :parma q0: float, central joint angle (rad)
         :param q1: float, elbow joint angle (rad)
         :param dt: float, time step
@@ -48,7 +46,7 @@ class ReacherEnvController():
             d_control_output = self.Kd @ d_control_input
         else:
             d_control_output = self.Kd * d_control_input
-        self.state_err_memory = state_err   # update register
+        self._state_error_memory = state_err   # update register
 
         # combine PD
         state_delta += d_control_output
@@ -56,4 +54,32 @@ class ReacherEnvController():
         # kinematics
         j_mat = getJacobian(q0, q1)[:2, :2]
         q_delta = (np.linalg.pinv(j_mat) @ state_err.reshape(2, -1))[:, 0]
+        
+        return q_delta
+
+    def pdControlJoint(self, q_err, dt):
+        """
+        PD controller using teh error in the end-effector
+
+        :param q_err: np array (len=2), joint angle error
+        :param dt: float, time step
+        :return q_delta: np array (len=2), joint angle difference
+        """
+        # P control
+        if isinstance(self.Kp, np.ndarray):
+            q_delta = self.Kp @ q_err
+        else:
+            q_delta = self.Kp * q_err
+
+        # D control
+        d_control_input = (q_err - self._q_error_memory) / dt
+        if isinstance(self.Kd, np.ndarray):
+            d_control_output = self.Kd @ d_control_input
+        else:
+            d_control_output = self.Kd * d_control_input
+        self._q_error_memory = q_err   # update register
+
+        # combine PD
+        q_delta += d_control_output
+
         return q_delta
--- ECE276C_HW1/p1_main.py
+++ ECE276C_HW1/p1_main.py
@@ -8,6 +8,7 @@ Created on Mon Oct  7 11:52:05 2019
 """
 import logging
 import numpy as np
+from matplotlib import pyplot as plt
 
 import gym
 import pybulletgym.envs
@@ -29,6 +30,24 @@ def getTrajectory(theta):
     y = (0.19 + 0.02 * np.cos(4 * theta)) * np.sin(theta)
     return x, y
 
+def plotHelper(traj_ref, traj_control, title):
+    """
+    Plots for Question 1
+
+    :param traj_ref: np ndarray, reference trajectory
+    :param traj_control: np ndarray, real trajectory
+    :param title: plot title
+    """
+    assert isinstance(title, str)
+    plt.figure()
+    plt.gca().set_aspect('equal')
+    plt.plot(traj_ref[:, 0], traj_ref[:, 1])
+    plt.plot(traj_control[:, 0], traj_control[:, 1])
+    plt.title(title)
+    plt.legend(['ref traj', 'real traj'])
+    plt.show()
+    # TODO: save fig
+
 
 if __name__ == '__main__':
     # get trajectory
@@ -48,8 +67,6 @@ if __name__ == '__main__':
     robot = ReacherEnv(env)
 
     # define gains
-    # Kp = 1
-    # Kd = 10
     Kp = np.diag([1, 1])
     Kd = np.diag([10, 10])
 
@@ -74,15 +91,46 @@ if __name__ == '__main__':
         # save trajectory
         traj_control.append(state_now.tolist())
 
-    traj_control = np.array(traj_control)
     # plot
-    plt.figure()
-    plt.gca().set_aspect('equal')
-    plt.plot(traj[:, 0], traj[:, 1])
-    plt.plot(traj_control[:, 0], traj_control[:, 1])
-    plt.title('Question 1.4 End Effector PD Control')
-    plt.legend(['ref traj', 'real traj'])
-    plt.show()
+    traj_control = np.array(traj_control)
+    plotHelper(traj_ref=traj, traj_control=traj_control, title='Question 1.4 End Effector Position PD Control')
 
     # Problem 5
-    # TODO
+    # reset environment
+    env.reset()
+    robot = ReacherEnv(env)
+
+    # define gains
+    Kp = np.diag([1, 1])
+    Kd = np.diag([1e-5, 1e-5])
+
+    # Problem 4
+    controller = ReacherEnvController(Kp, Kd)
+    traj_control = []
+    for theta in np.arange(-np.pi, np.pi, dt):
+        state_ref = getTrajectory(theta)
+        q_ref = getIK(state_ref[0], state_ref[1])
+        q0, _, q1, _ = robot.getJointPositionAndVelocity()
+
+        # error
+        q_err = q_ref - np.array([q0, q1])
+
+        # PD control
+        q_delta = controller.pdControlJoint(q_err, dt)
+
+        # robot action
+        q0, q1 = q0 + q_delta[0], q1 + q_delta[1]
+
+        # unwrap
+        q0, q1 = q0 % (2 * np.pi), q1 % (2 * np.pi)
+
+        # robot action
+        robot.setJointPosition(q0, q1)
+
+        # save trajectory
+        traj_control.append(getForwardModel(q0, q1)[:2].tolist())
+
+    traj_control = np.array(traj_control)
+    # plot
+    traj_control = np.array(traj_control)
+    plotHelper(traj_ref=traj, traj_control=traj_control, title='Question 1.5 Joint Angle PD Control')
