eb54ea028e8304476e324437b04d5a11bddfdb48
Mingwei Xu <xumw1996@wireless-169-228-76-17.ucsd.edu>
2019-10-13 22:25:46 -0700

    tunning question 2 speed control

--- ECE276C_HW1/p2_main.py
+++ ECE276C_HW1/p2_main.py
@@ -33,10 +33,17 @@ def kinematic_model(pos_ref, pos_now, theta_now):
     x, y = pos_now[0], pos_now[1]
 
     gamma = np.arctan2((y_d - y), (x_d - x)) - theta_now
+    if gamma <= - np.pi:
+        gamma += 2 * np.pi
+    elif gamma >= np.pi:
+        gamma -= 2 * np.pi
+    # gamma = np.arctan((y_d - y) / (x_d - x)) - theta_now
     return gamma
 
 if __name__ == '__main__':
     env = SDRaceCar(render_env=True, track="FigureEight")
+    # env = SDRaceCar(render_env=True, track="Linear")
+    # env = SDRaceCar(render_env=True, track="Circle")
     env.reset()
     env.render()
 
@@ -47,14 +54,14 @@ if __name__ == '__main__':
     wheel_distance = env.l_f + env.l_r
 
     # define
-    Kp = 1e-1
+    Kp = 1e-2
 
     for i in range(500):
         # entering control loop
         state_obs = env.get_observation()
         state_ref = state_obs[-1]     # desired position
         
-        x_now, y_now, theta_now = state_obs[0], state_obs[1], state_obs[2]
+        [x_now, y_now, theta_now, v_x, v_y] = state_obs[:5]
         
         dx, dy = state_ref[0] - x_now, state_ref[1] - y_now
 
@@ -64,22 +71,28 @@ if __name__ == '__main__':
                                   [np.sin(theta_now), np.cos(theta_now)]]),
                         np.array([dx, dy]))
         print('d_body: ', d_body)
+        vel = np.sqrt(v_x ** 2 + v_y ** 2)
+        print('vel: ', vel)
 
         print('gamma:', gamma)
-        if abs(gamma) >= 0.2:
-            thrust = -1 + np.random.rand() * 0.1
+
+        if i < 10:
+            thrust = 1  # launch control
+        # elif vel > 5:
+        elif abs(v_x) > 10
+            thrust = -1     # speed penalty
         else:
-            thrust = Kp * (d_body[1] ** 2 - d_body[0] ** 2)     # TODO
-        # regularize
-        thrust -= 0.5
-        thrust = max(thrust, -0.9)
-        thrust = min(thrust, 0.5)
-        # if i < 10:  # TODO
-        #     thrust = 1
-        # elif i < 50:
-        #     thrust = -0.5
-        # else:
-        #     thrust = - 1
+            if abs(gamma) >= 0.15:
+                thrust = -1 + np.random.rand() * 0.05
+            else:
+                thrust = Kp * (d_body[1] ** 2 - d_body[0] ** 2)     # TODO
+            # speed limit
+            thrust -= 0.5
+            print('thrust before speed limit: ', thrust)
+            thrust = max(thrust, -1)      # lower limit
+            thrust = min(thrust, -0.5)      # upper limit
+
+        # action
         env.step([gamma, thrust])
         print('i: {}, input (gamma, thrust): {}'.format(i, [gamma, thrust]))
         env.render()
