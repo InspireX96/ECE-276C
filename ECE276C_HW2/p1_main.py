bc3823fec2489f9ccf04396de9742dbd426cf390
Mingwei Xu <xumw1996@wireless-169-228-92-183.ucsd.edu>
2019-10-18 16:06:54 -0700

    clean up p1

--- ECE276C_HW2/p1_main.py
+++ ECE276C_HW2/p1_main.py
@@ -16,39 +16,49 @@ def toyPolicy(state):
     return (state + 1) % 4
 
 
-def testPolicy(policy):
+def testPolicy(env, policy, trials=100, verbose=False):
     """
     Test policy, return averate rate of successful episodes
     over 100 trials.
 
-    Note: use global variable env 
-
+    :param env: object, gym environment
     :param policy: function, a deterministic policy that takes state as input,
                              output action in (0, 1, 2, 3, 4)
+    :param trials: int (>0), number of trials to test policy, defaults to 100
+    :param verbose: bool, flag to print more infomation and render final state
     """
-    trials = 100    # 100 trials
-    success = np.zeros(trials)  # array to record success/fail
+    assert isinstance(trials, int) and trials > 0
+    success = 0
 
     for i in range(trials):
         state = env.reset()     # reset env
         done = False
         while not done:
             action = policy(state)
-            state, reward, done, info = env.step(action)
+            state, _, done, _ = env.step(action)
         if state == 15:
-            success[i] = 1
+            success += 1    # successfully reach goal
+        if verbose:
+            print('\n**trial {} final state: {}**'.format(i, state))
+            env.render()
 
-    print('success rate: ', np.sum(success) / trials)
+    if verbose:
+        print('success rate: {} ({} out of {})'.format(
+            success / trials, success, trials))
+    else:
+        print('success rate: {}'.format(success / trials))
 
 
 def learnModel(samples=5):
     """
     Learn model
+
+    :param samples: int, random samples
     """
     assert isinstance(samples, int) and samples > 0
     state = env.reset()
-    action = np.random.randint(4)   # random action
-    state_next, reward, done, info = env.step(action)
+    action = env.action_space.sample()   # random action
+    state_next, reward, done, _ = env.step(action)
     print(state, state_next, reward)
     # TODO: wait for piazza clarification
 
@@ -70,8 +80,8 @@ def valueIter():
 if __name__ == '__main__':
     env = gym.make('FrozenLake-v0')
     env.reset()
-    env.render()
 
     # Question 1.3
-    testPolicy(toyPolicy)
+    print('\n===== Question 1.3 =====\n')
+    testPolicy(env, toyPolicy, trials=100, verbose=False)
     # state, reward, done, info = env.step()
