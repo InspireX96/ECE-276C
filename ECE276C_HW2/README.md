23d4c30733020117757218c3968efecdbb95cd1e
Mingwei Xu <xumw1996@wireless-169-228-92-183.ucsd.edu>
2019-10-18 12:37:13 -0700

    working on hw2

---
+++ ECE276C_HW2/README.md
@@ -0,0 +1,19 @@
+# ECE 276C HW2
+
+Mingwei Xu A53270271
+
+## Note for env
+
+```
+Environment state :
+    " 0 , 1 , 2 , 3 ",
+    " 4 , 5 , 6 , 7 ",
+    " 8 , 9 , 10, 11",
+    " 12, 13, 14, 15",
+Corresponding to
+    "SFFF",
+    "FHFH",
+    "FFFH",
+    "HFFG"
+Action "0, 1, 2, 3" is "left, down, right, up".
+```
---
+++ ECE276C_HW2/p1_main.py
@@ -0,0 +1,58 @@
+"""
+ECE 276C HW2 Question 1
+"""
+
+import gym
+import numpy as np
+
+
+def toyPolicy(state):
+    """
+    Toy policy in problem 3
+    
+    :param state: current state
+    :return: int, policy (LEFT = 0, DOWN = 1, RIGHT = 2, UP = 3)
+    """
+    return (state + 1) % 4
+
+
+def testPolicy(policy):
+    """
+    Test policy, return averate rate of successful episodes
+    over 100 trials.
+    
+    Note: use global variable env 
+    
+    :param policy: function, a deterministic policy that takes state as input,
+                             output action in (0, 1, 2, 3, 4)
+    """
+    trials = 100    # 100 trials
+    success = np.zeros(trials)  # array to record success/fail
+
+    for i in range(trials):
+        state = env.reset()     # reset env
+        done = False
+        while not done:
+            action = policy(state)
+            state, reward, done, info = env.step(action)
+        if state == 15:
+            success[i] = 1
+    
+    print('success rate: ', np.sum(success) / trials)
+
+
+def learnModel(samples=1e5):
+    """
+    Learn model
+    """
+    assert isinstance(samples, int) and samples > 0
+    pass
+
+
+if __name__ == '__main__':
+    env = gym.make('FrozenLake-v0')
+    env.reset()
+    env.render()
+
+    testPolicy(toyPolicy)
+    # state, reward, done, info = env.step()
