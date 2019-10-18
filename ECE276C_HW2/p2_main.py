7aa245538708adfafdfc926c950cc10d7d5867a8
Mingwei Xu <xumw1996@wireless-169-228-92-183.ucsd.edu>
2019-10-18 16:20:11 -0700

    clean up q2

--- ECE276C_HW2/p2_main.py
+++ ECE276C_HW2/p2_main.py
@@ -1,10 +1,9 @@
 """
 ECE 276C HW2 Question 2
 """
-import logging
 import gym
 import numpy as np
-
+from p1_main import testPolicy
 
 class QlearningPolicy(object):
     """
@@ -23,13 +22,26 @@ class QlearningPolicy(object):
         self.gamma = gamma
 
         # get state and action space
-        self.state_space = np.arange(env.observation_space.n)
-        self.action_space = np.arange(env.action_space.n)
+        self.state_space_n = env.observation_space.n
+        self.action_space_n = env.action_space.n
 
         # init Q function
-        self.Q_table = np.random.rand(
-            env.observation_space.n, env.action_space.n)
-        logging.warning('Q table shape: {}'.format(self.Q_table.shape))
+        self.Q_table = np.zeros((
+            self.state_space_n, self.action_space_n))
+
+    def _takeAction(self, state, episode):
+        """
+        Take action with epsilon-greedy (with probability 1 - episode/5000)
+
+        :param state: int, current state
+        :param episode: int, current episode
+        :return: int, action
+        """
+        if np.random.rand() <= 1 - episode / 5000:
+            # greedy
+            return self.env.action_space.sample()
+        # not greedy
+        return np.argmax(self.Q_table[state, :])
 
     def updateQ(self, state, state_next, action, reward):
         """
@@ -40,44 +52,58 @@ class QlearningPolicy(object):
         :param action: int, action
         :param reward: float, reward
         """
-        Q_current = self.Q_table[state][action]
-        Q_next = self.Q_table[state]    # this is array
+        Q_current = self.Q_table[state, action]
+        Q_next = self.Q_table[state_next, :]    # this is array
 
         # update Q function
-        self.Q_table[state][action] += self.alpha * \
+        self.Q_table[state, action] += self.alpha * \
             (reward + self.gamma * np.max(Q_next) - Q_current)
 
-    def _takeAction(self, state, episode):
-        """
-        Take action with epsilon-greedy
-        
-        :param state: [description]
-        :param episode: [description]
-        """
-        pass
-
-    def trainPolicy(self, episode=5000):
+    def trainPolicy(self, max_episode=5000, verbose=False):
         """
         Train policy
 
-        :param episode: int (>0), training episode
+        :param max_episode: int (>0), training episode, defaults to 5000
+        :param verbose: bool, flag to print more infomation and render final state
+        :return: a lambda policy function, take state as input and output action
         """
-        assert isinstance(episode, int) and episode > 0
-        
-        for i in range(episode):
+        assert isinstance(max_episode, int) and max_episode > 0
+
+        for i in range(max_episode):
             state = self.env.reset()
             done = False
-            pass
-
-    def testPolicy(self):
-        """
-        Test policy
-        """
-        pass
+            steps = 0
+            while not done:
+                action = self._takeAction(state=state, episode=i)
+                state_next, reward, done, _ = self.env.step(action)
+                self.updateQ(state=state, state_next=state_next,
+                             action=action, reward=reward)
+                state = state_next
+                steps += 1
+        
+        if verbose:
+            print('Episode {} finished in {} steps'.format(i, steps))
+        return lambda state: np.argmax(self.Q_table[state, :])
 
 
 if __name__ == '__main__':
     env = gym.make('FrozenLake-v0')
     env.reset()
-    env.render()
-    policy = QlearningPolicy(env, alpha=0.05, gamma=0.99)
+
+    # Q 2.1(a)
+    print('\n===== Question 2.1(a) =====\n')
+    gamma = 0.99
+    for alpha in [0.05, 0.1, 0.25, 0.5]:
+        print('alpha: {}, gamma: {}'.format(alpha, gamma))
+        qlearn = QlearningPolicy(env, alpha=alpha, gamma=gamma)
+        policy = qlearn.trainPolicy()
+        testPolicy(env, policy, trials=100, verbose=False)
+
+    # Q 2.1(b)
+    print('\n===== Question 2.1(b) =====\n')
+    alpha = 0.05
+    for gamma in [0.9, 0.95, 0.99]:
+        print('alpha: {}, gamma: {}'.format(alpha, gamma))
+        qlearn = QlearningPolicy(env, alpha=alpha, gamma=gamma)
+        policy = qlearn.trainPolicy()
+        testPolicy(env, policy, trials=100, verbose=False)
